313c3b3a1fabba4d64310543e9467597e06e5793
AlexeyBelov <bel0valeksey@yandex.ru>
2024-01-10 21:08:21 +0300

    Refactoring: - change class names - change DI from private field autowiring to constructor autowiring - change package names - delete unused class - exclude Configuration.class

--- src/main/java/com/belov/jdbc/config/ProductConfiguration.java
+++ src/main/java/com/belov/jdbc/config/ProductConfiguration.java
@@ -1,30 +1,30 @@
-package com.belov.jdbc.config;
-
-import com.belov.jdbc.repository.ProductRepository;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.stream.Collectors;
-
-@Configuration
-public class ProductConfiguration {
-
-    @Bean
-    ProductRepository repository() {
-        return new ProductRepository(read("search_by_name.sql"));
-    }
-
-    private static String read(String scriptFileName) {
-        try (InputStream is = new ClassPathResource(scriptFileName).getInputStream();
-             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is))) {
-            return bufferedReader.lines().collect(Collectors.joining("\n"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
+//package com.belov.jdbc.config;
+//
+//import com.belov.jdbc.repository.ProductRepository;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.ClassPathResource;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.util.stream.Collectors;
+//
+//@Configuration
+//public class ProductConfiguration {
+//
+//    @Bean
+//    ProductRepository repository() {
+//        return new ProductRepository(read("search_by_name.sql"));
+//    }
+//
+//    private static String read(String scriptFileName) {
+//        try (InputStream is = new ClassPathResource(scriptFileName).getInputStream();
+//             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is))) {
+//            return bufferedReader.lines().collect(Collectors.joining("\n"));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
--- src/main/java/com/belov/jdbc/repository/ProductRepository.java
+++ src/main/java/com/belov/jdbc/repository/ProductRepository.java
@@ -1,12 +1,18 @@
 package com.belov.jdbc.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//@Repository
+@Repository
 public class ProductRepository {
 
     private String searchByName;
@@ -14,8 +20,17 @@ public class ProductRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public ProductRepository(String searchByName) {
-        this.searchByName = searchByName;
+//    public ProductRepository(String searchByName) {
+//        this.searchByName = searchByName;
+//    }
+
+    public ProductRepository() {
+        try (InputStream is = new ClassPathResource("search_by_name.sql").getInputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is))) {
+            this.searchByName = bufferedReader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getProductName(String name) {
